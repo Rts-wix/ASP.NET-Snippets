9d0c8e9ab28fe3469147fd318cfee968d7351707
soren <soren@magnussons.dk>
2012-08-29 22:03:35 -0700

    startup

---
+++ SeasonImage/Default.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .style1
+        {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+      
+        <h1>
+            Sommertid eller vintertid:</h1>
+      
+        <asp:Image ID="Image1" runat="server" Width="200px" />
+        <p>
+        Så er det jo bare at vente til det skifter...
+
+        </p>
+        <p>
+            Eller vi kan teste lidt mere systematisk:</p>
+        <table class="style1">
+            <tr>
+                <td>
+                    <asp:Image ID="ImageSommer" runat="server" Width="200px" />
+                </td>
+                <td>
+                    <asp:Image ID="ImageVinter" runat="server" Width="200px" />
+        
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    bør være sommer</td>
+                <td>
+        bør være vinter</td>
+            </tr>
+        </table>
+&nbsp;<p>
+            I Page_Load checker jeg om metoden sommertid() returnerer true (det gør den når 
+            DateTime.Now.IsDaylightSavingTime() er true). Der er en ekstra 
+            sommertid(DateTime currentDate), den hør at jeg kan teste med en fast dato.</p>
+        <p>
+            Det er i det hele taget lidt tidskrævende at afprøve den funktion, Det ville 
+            faktisktage op til et år at sikre sig at alle måneder er dækket. Så vi må finde 
+            på noget</p>
+        <h1>
+        De fire årstider:</h1>
+        Her bruger vi metoden <em>skiftImageUrlEfterAarstid(Image image, DateTime iDag)</em>, 
+        og <em>ImageLoadAarstid(object sender, EventArgs e)</em>.<br />
+        Den første laver arbejdet, den anden er en eventHandler som kan sættes direkte 
+        på asp:Iamge-taget.<br />
+        <br />
+        I eksemplet er der også oprettet en Page_LoadCompleted. Det er kun for at kunne 
+        afprøve om skiftImageUrlEfterAarstid() virker. Page_LoadCompleted udføres efter 
+        Page_Load OG EFTER eventhandlere på sidens øvrige controls. ImageLoadAarstid 
+        sætter jo ellers et andet billede ind i stedet for det jeg ville teste, hvis jeg 
+        tester fra Page_Load.<br />
+        <table class="style1">
+            <tr>
+                <td>
+                    <asp:Image ID="ImageForaar2" runat="server" Width="200px" onload="ImageLoadAarstid" />
+                </td>
+                <td>
+                    <asp:Image ID="ImageSommer2" runat="server" Width="200px" onload="ImageLoadAarstid"/>
+                </td>
+                <td>
+                    <asp:Image ID="ImageEfteraar2" runat="server" Width="200px" onload="ImageLoadAarstid" />
+                </td>
+                <td>
+                    <asp:Image ID="ImageVinter2" runat="server" Width="200px" onload="ImageLoadAarstid" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+        forår          </td>
+                <td>
+        sommer 
+                </td>
+                <td>
+        efterår 
+                </td>
+                <td>
+        vinter 
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ SeasonImage/Default.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      
+        // afprøver sommer tid()
+        if (sommertid())
+        {
+            Image1.ImageUrl = "billeder/Desert.jpg";
+        }
+        else
+        {
+            Image1.ImageUrl = "billeder/Penguins.jpg";
+        }
+
+        if (sommertid(new DateTime(2012, 12, 24, new System.Globalization.GregorianCalendar()))) //System.Globalization.GregorianCalendarTypes.Localized) ))
+        {
+            ImageVinter.ImageUrl = "billeder/Desert.jpg";
+        }
+        else
+        {
+            ImageVinter.ImageUrl = "billeder/Penguins.jpg";
+        }
+
+        if (sommertid(new DateTime(2012, 06, 24, new System.Globalization.GregorianCalendar()))) //System.Globalization.GregorianCalendarTypes.Localized) ))
+        {
+            ImageSommer.ImageUrl = "billeder/Desert.jpg";
+        }
+        else
+        {
+            ImageSommer.ImageUrl = "billeder/Penguins.jpg";
+        }
+        
+        ////Dårlig afprøvning afprøvning årstid
+        // prøv envtuelt af fjerne kommentæringen her fra og kommentere Page_LoadCompleted ud. Det duer ikke!
+        //skiftImageUrlEfterAarstid(ImageForaar2, new DateTime(2013, 03, 01));
+        //skiftImageUrlEfterAarstid(ImageSommer2, new DateTime(2013, 06, 01));
+        //skiftImageUrlEfterAarstid(ImageEfteraar2, new DateTime(2013, 09, 01));
+        //skiftImageUrlEfterAarstid(ImageVinter2, new DateTime(2013, 12, 01));
+
+    }
+
+    // Den her er kun med for at vi kan fuske med dato'erne og afprøve om billederne skifter
+    protected void Page_LoadComplete(object sender, EventArgs e)
+    {
+        skiftImageUrlEfterAarstid(ImageForaar2, new DateTime(2013, 03, 01));
+        skiftImageUrlEfterAarstid(ImageSommer2, new DateTime(2013, 06, 01));
+        skiftImageUrlEfterAarstid(ImageEfteraar2, new DateTime(2013, 09, 01));
+        skiftImageUrlEfterAarstid(ImageVinter2, new DateTime(2013, 12, 01));
+
+    }
+ 
+
+    /// <summary>
+    /// Er det sommertid, eller ej?
+    /// </summary>
+    /// <param name="currentDate">Angiv datoen der skal checkes</param>
+    /// <returns>true/false</returns>
+    protected static bool sommertid(DateTime currentDate)   
+    {
+        bool result = false;
+
+        result = currentDate.IsDaylightSavingTime();
+
+        return result;
+    }
+
+    /// <summary>
+    /// Er det sommer tid?
+    /// Bruger dags dato som default værdi
+    /// </summary>
+    /// <returns>sommertid? true/false</returns>
+    protected static bool sommertid()
+    {
+        return sommertid( DateTime.Now );
+    }
+
+    /// <summary>
+    /// EventHandler som sættes på asp:Image controls.
+    /// Skifter billedes url v.h.a skiftImageUrlEfterAarstid
+    /// </summary>
+    /// <param name="sender">Det objekt der udløser eventet. 
+    /// Af typen object ( for at være kompatibel med ALLE controls. Her er det altid af type Image, så vi laver et typeCast.</param>
+    /// <param name="e">bruges ikke her</param>
+    protected void ImageLoadAarstid(object sender, EventArgs e)
+    {
+        Image image = ((Image)sender);
+        DateTime iDag = DateTime.Now;
+
+        skiftImageUrlEfterAarstid(image, iDag);
+    }
+
+    /// <summary>
+    /// Laer selve skitet af billedets url.
+    /// Indeler året i fire årstider, med tre måneder i hver. Tilpas det som du ønsker.
+    /// </summary>
+    /// <param name="image">billedet som skal skiftes</param>
+    /// <param name="iDag">dags dato</param>
+    private static void skiftImageUrlEfterAarstid(Image image, DateTime iDag)
+    {
+        switch (iDag.Month)
+        {
+            // vinter
+            case 12:
+            case 1:
+            case 2:
+                image.ImageUrl = "billeder/Penguins.jpg";
+                break;
+            // forår
+            case 3:
+            case 4:
+            case 5:
+                image.ImageUrl = "billeder/Tulips.jpg";
+                break;
+            // sommer
+            case 6:
+            case 7:
+            case 8:
+                image.ImageUrl = "billeder/Desert.jpg";
+                break;
+            // efterår
+            case 9:
+            case 10:
+            case 11:
+                image.ImageUrl = "billeder/Lighthouse.jpg";
+                break;
+            default:
+                image.ImageUrl = "billeder/nopicture.jpg";
+                break;
+        }
+    }
+}
